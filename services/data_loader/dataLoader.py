afc6e2bc47e4462822b60c5a94701e0ef087a8d7
Chaim Shlez <chaimshlez@gmail.com>
2025-08-18 10:13:35 +0300

    mongo

--- services/data_loader/dataLoader.py
+++ services/data_loader/dataLoader.py
@@ -9,19 +9,17 @@ class ConnectionWrapper:
             port=int(os.getenv("MONGODB_PORT","27017" )),
             username=os.getenv("MONGODB_USER"),
             password=os.getenv("MONGODB_PASSWORD"),
-            authSource=os.getenv("MONGODB_DATABASE")
+            authSource="admin"
+
+
         )
         self.db = self.client[os.getenv("MONGODB_DATABASE")]
 
     def get_all(self, collection_name):
-        print(os.getenv("MONGODB_HOST"))
-        print(os.getenv("MONGODB_PORT"))
-        print(os.getenv("MONGODB_USER"))
-        print(os.getenv("MONGODB_PASSWORD"))
-        print(os.getenv("MONGODB_DATABASE"))
+
         try:
             collection = self.db[collection_name]
-            return collection.find_one()
+            return list(collection.find({}))
 
 
         except Exception as e:
