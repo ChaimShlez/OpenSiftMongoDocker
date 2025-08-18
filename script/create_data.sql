dce537d3a45401b01fa61094c345c88ee1304724
Chaim Shlez <chaimshlez@gmail.com>
2025-08-18 19:37:58 +0300

    mongo

--- script/create_data.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE students (
-   id INT 				AUTO_INCREMENT,
-   first_name VARCHAR(20) 	NOT NULL,
-   last_name VARCHAR(20) 	NOT NULL,
-   PRIMARY KEY (id)
-);
--- script/insert_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO students (first_name ,last_name)
-VALUES ("chaim" ,"shlez"),
-VALUES ("dan" ,"shachar"),
-VALUES ("avi" ,"levi"),
-VALUES ("idan" ,"shay"),
-VALUES ("yaniv" ,"adar");
